7e70e162e5b073663a18ed5fb72a27ca9d1beee9
NicolaAnnu <nicolaannunziata2000@gmail.com>
2026-04-03 22:40:52 +0200

    add 2 kernel

--- Personal/main.py
+++ Personal/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
     x_train, y_train, _, _ = load_mnist()
 
-    image = x_train[0][0]  
+    image = x_train[0][0]
 
     print("Input shape:", image.shape)
     plt.imshow(image, cmap='gray')
@@ -15,33 +15,51 @@ def main():
 
     # kernel (3x3)
     kernel = np.random.randn(3, 3)
+    kernel2 = np.random.randn(3, 3)
 
     conv_layer = Convolution(image, kernel)
 
-    #  convoluzione
     conv_out = conv_layer.conv()
-    print("After conv:", conv_out.shape)
+    print("After conv1:", conv_out.shape)
     plt.imshow(conv_out, cmap='gray')
-    plt.title("After Convolution")
+    plt.title("After Convolution 1")
     plt.show()
 
-    #  ReLU
     relu_out = conv_layer.relu(conv_out)
-    print("After ReLU:", relu_out.shape)
+    print("After ReLU1:", relu_out.shape)
     plt.imshow(relu_out, cmap='gray')
-    plt.title("After ReLU")
+    plt.title("After ReLU 1")
     plt.show()
 
-    #  pooling
     pool_out = conv_layer.max_pooling(relu_out)
-    print("After pooling:", pool_out.shape)
+    print("After pooling1:", pool_out.shape)
     plt.imshow(pool_out, cmap='gray')
-    plt.title("After Pooling")
+    plt.title("After Pooling 1")
     plt.show()
 
-    print("\nSample output (top-left corner):")
-    print(pool_out[:5, :5])
+    # ---------- SECOND BLOCK ----------
+    conv_layer2 = Convolution(pool_out, kernel2)
 
+    conv_out2 = conv_layer2.conv()
+    print("After conv2:", conv_out2.shape)
+    plt.imshow(conv_out2, cmap='gray')
+    plt.title("After Convolution 2")
+    plt.show()
+
+    relu_out2 = conv_layer2.relu(conv_out2)
+    print("After ReLU2:", relu_out2.shape)
+    plt.imshow(relu_out2, cmap='gray')
+    plt.title("After ReLU 2")
+    plt.show()
+
+    pool_out2 = conv_layer2.max_pooling(relu_out2)
+    print("After pooling2:", pool_out2.shape)
+    plt.imshow(pool_out2, cmap='gray')
+    plt.title("After Pooling 2")
+    plt.show()
+
+    print("\nSample output after second pooling:")
+    print(pool_out2)
 
 if __name__ == "__main__":
     main()
